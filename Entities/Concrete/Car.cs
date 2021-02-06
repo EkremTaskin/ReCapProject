588889579f12635bb16a11cd360c68378a1e8f94
EkremTaskin <eko20042004@hotmail.com>
2021-02-06 20:27:13 +0300

    06.02.2021

--- Entities/Concrete/Car.cs
+++ Entities/Concrete/Car.cs
@@ -9,18 +9,14 @@ namespace Entities.Concrete
     {
         public int Id { get; set; }
 
-<<<<<<< HEAD
-		public int BrandId { get; set; }
+        public int BrandId { get; set; }
 
-		public int ColorId { get; set; }
+        public int ColorId { get; set; }
 
-		public int ModelYear { get; set; }
-=======
         public int ModelYear { get; set; }
->>>>>>> c800f627bf9f33000f414da30df7f42daa81783f
 
         public decimal DailyPrice { get; set; }
 
         public string Description { get; set; }
-	}
+    }
 }
